7a481ad9e6c7305f76a2de687801ac00cf6bce1e
Dennis Reichenberg <9849919+SoylentBob@users.noreply.github.com>
2022-12-18 16:44:32 +0100

    Implemented a CreateToDoPresenterTest
    https://github.com/flens-dev/pendenzenliste/issues/7

---
+++ app-vaadin/src/test/java/pendenzenliste/vaadin/CreateToDoPresenterTest.java
@@ -0,0 +1,36 @@
+package pendenzenliste.vaadin;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Test;
+import pendenzenliste.ports.out.ToDoCreatedResponse;
+import pendenzenliste.ports.out.ToDoCreationFailedResponse;
+
+class CreateToDoPresenterTest
+{
+
+  @Test
+  public void handleSuccessfulResponse()
+  {
+    final var view = mock(ToDoView.class);
+    final var presenter = new CreateToDoPresenter(view);
+
+    presenter.handleSuccessfulResponse(new ToDoCreatedResponse("test-identity"));
+
+    verify(view, times(1)).clearEditor();
+    verify(view, times(1)).loadToDos();
+  }
+
+  @Test
+  public void handleFailedResponse()
+  {
+    final var view = mock(ToDoView.class);
+    final var presenter = new CreateToDoPresenter(view);
+
+    presenter.handleFailedResponse(new ToDoCreationFailedResponse("Something bad happened"));
+
+    verify(view, times(1)).showGenericErrorMessage("Something bad happened");
+  }
+}

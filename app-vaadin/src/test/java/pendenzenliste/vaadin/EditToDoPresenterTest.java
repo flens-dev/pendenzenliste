0dab880b1fa6a1a5189acad9b81b6e13f711f03f
Dennis Reichenberg <9849919+SoylentBob@users.noreply.github.com>
2022-12-18 16:49:14 +0100

    Implemented the EditToDoPresenterTest
    https://github.com/flens-dev/pendenzenliste/issues/7

---
+++ app-vaadin/src/test/java/pendenzenliste/vaadin/EditToDoPresenterTest.java
@@ -0,0 +1,51 @@
+package pendenzenliste.vaadin;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+import org.junit.jupiter.api.Test;
+import pendenzenliste.ports.out.FetchToDoFailedResponse;
+import pendenzenliste.ports.out.ToDoFetchedResponse;
+
+class EditToDoPresenterTest
+{
+
+  @Test
+  public void handleFailedResponse()
+  {
+    final var view = mock(ToDoView.class);
+    final var presenter = new EditToDoPresenter(view);
+
+    presenter.handleFailedResponse(new FetchToDoFailedResponse("Something bad happened"));
+
+    verify(view, times(1)).showGenericErrorMessage("Something bad happened");
+  }
+
+  @Test
+  public void handleSuccessfulResponse()
+  {
+    final var view = mock(ToDoView.class);
+    final var presenter = new EditToDoPresenter(view);
+
+    presenter.handleSuccessfulResponse(new ToDoFetchedResponse(
+        "test-identity",
+        "test-headline",
+        "test-description",
+        LocalDateTime.now(),
+        LocalDateTime.now(),
+        LocalDateTime.now(),
+        "DONE",
+        List.of("UPDATE")
+    ));
+
+    verify(view, times(1)).setSelectedToDo(new ToDoViewModel(
+        "test-identity",
+        "test-headline",
+        "test-description"
+    ));
+  }
+}

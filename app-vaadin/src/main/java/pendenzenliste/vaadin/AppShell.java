a9860d7f353dacbeb5b3ef3b4a3bce79f3c60985
Dennis Reichenberg <9849919+SoylentBob@users.noreply.github.com>
2022-12-20 00:10:14 +0100

    Enabled push on the AppShell
    https://github.com/flens-dev/pendenzenliste/issues/18

--- app-vaadin/src/main/java/pendenzenliste/vaadin/AppShell.java
+++ app-vaadin/src/main/java/pendenzenliste/vaadin/AppShell.java
@@ -1,12 +1,14 @@
 package pendenzenliste.vaadin;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.theme.Theme;
 
 /**
  * The app shell used to run the application.
  */
+@Push
 @Theme(value = "pendenzenliste")
 @PWA(name = "Pendenzenliste", shortName = "Pendenzenliste")
 public class AppShell implements AppShellConfigurator

7ceef22afe8a748157bb4b71d539f2122bd22ab3
Dennis Reichenberg <9849919+SoylentBob@users.noreply.github.com>
2022-12-21 12:49:46 +0100

    Added relevant ToDoEvent types
    https://github.com/flens-dev/pendenzenliste/issues/9

---
+++ domain/src/main/java/pendenzenliste/domain/ToDoEvent.java
@@ -0,0 +1,23 @@
+package pendenzenliste.domain;
+
+import java.time.LocalDateTime;
+
+/**
+ * The common interface for objects that represent some kind of event in the context of a domain.
+ */
+public interface ToDoEvent
+{
+  /**
+   * The timestamp when the event occurred.
+   *
+   * @return The timestamp.
+   */
+  LocalDateTime timestamp();
+
+  /**
+   * The identity of the todo that the event belongs to.
+   *
+   * @return The identity of the todo.
+   */
+  IdentityValueObject identity();
+}
---
+++ domain/src/main/java/pendenzenliste/domain/ToDoUpdatedEvent.java
@@ -0,0 +1,14 @@
+package pendenzenliste.domain;
+
+import java.time.LocalDateTime;
+
+/**
+ * An event that can be used to indicate that a todo has been updated.
+ *
+ * @param timestamp The timestamp.
+ * @param identity  The identity.
+ */
+public record ToDoUpdatedEvent(LocalDateTime timestamp, IdentityValueObject identity)
+    implements ToDoEvent
+{
+}

757789171cb4a1e17c63cc4f953c77a2ecf0fedc
Dennis Reichenberg <9849919+SoylentBob@users.noreply.github.com>
2023-01-07 09:37:15 +0100

    Added missing javadocs
    https://github.com/flens-dev/pendenzenliste/issues/9

--- domain/supporting-domain-achievements/src/main/java/pendenzenliste/domain/achievements/AchievementEntity.java
+++ domain/supporting-domain-achievements/src/main/java/pendenzenliste/domain/achievements/AchievementEntity.java
@@ -3,13 +3,12 @@ package pendenzenliste.domain.achievements;
 /**
  * An entity that can be used to represent an achievement.
  *
+ * @param identity The identity of the achievement.
  * @param name     The name of the achievement.
  * @param state    The state of the achievement.
  * @param unlocked The timestamp that indicates when the achievement has been unlocked.
  */
-public record AchievementEntity(IdentityValueObject identity,
-                                AchievementValueType name,
-                                StateValueType state,
-                                UnlockedTimestampValueType unlocked)
+public record AchievementEntity(IdentityValueObject identity, AchievementValueType name,
+                                StateValueType state, UnlockedTimestampValueType unlocked)
 {
 }

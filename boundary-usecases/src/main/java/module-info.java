6ffd33ede3a1c642025f78e217b1e22e8d0a9aa0
Dennis Reichenberg <9849919+SoylentBob@users.noreply.github.com>
2023-09-13 07:00:42 +0200

    Removed rxjava dependency in boundary-usecases

--- boundary-usecases/src/main/java/module-info.java
+++ boundary-usecases/src/main/java/module-info.java
@@ -4,20 +4,18 @@ import pendenzenliste.todos.boundary.in.ToDoInputBoundaryFactoryProvider;
 import pendenzenliste.todos.usecases.ToDoUseCaseFactoryProvider;
 
 module pendenzenliste.usecases.main {
-  exports pendenzenliste.achievements.usecases;
-  exports pendenzenliste.todos.usecases;
+    exports pendenzenliste.achievements.usecases;
+    exports pendenzenliste.todos.usecases;
 
-  requires pendenzenliste.boundary.main;
-  requires pendenzenliste.core.domain.todos.main;
-  requires pendenzenliste.gateway.main;
-  requires pendenzenliste.messaging.main;
-  requires pendenzenliste.supporting.domain.achievements.main;
+    requires pendenzenliste.boundary.main;
+    requires pendenzenliste.core.domain.todos.main;
+    requires pendenzenliste.gateway.main;
+    requires pendenzenliste.messaging.main;
+    requires pendenzenliste.supporting.domain.achievements.main;
 
-  provides AchievementInputBoundaryFactoryProvider
-      with AchievementUseCaseFactoryProvider;
+    provides AchievementInputBoundaryFactoryProvider
+            with AchievementUseCaseFactoryProvider;
 
-  provides ToDoInputBoundaryFactoryProvider
-      with ToDoUseCaseFactoryProvider;
-
-  requires io.reactivex.rxjava3;
+    provides ToDoInputBoundaryFactoryProvider
+            with ToDoUseCaseFactoryProvider;
 }

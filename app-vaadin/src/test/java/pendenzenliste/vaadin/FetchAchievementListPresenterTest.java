61a43b0e67f1965fb419bd6fb52295f27dc15dfc
Dennis Reichenberg <9849919+SoylentBob@users.noreply.github.com>
2023-11-26 10:52:21 +0100

    Added FetchAchievementListPresenterTest

--- app-vaadin/src/test/java/pendenzenliste/vaadin/FetchAchievementListPresenterTest.java
+++ app-vaadin/src/test/java/pendenzenliste/vaadin/FetchAchievementListPresenterTest.java
@@ -32,4 +32,142 @@ class FetchAchievementListPresenterTest {
 
         assertions.assertAll();
     }
+
+    @Test
+    public void handleSuccessfulResponse_unlockedJourneyBeginsAchievement() {
+        final var viewModel = new ToDoListViewModel();
+        final var presenter = new FetchAchievementListPresenter(viewModel);
+
+        presenter.handleSuccessfulResponse(new FetchAchievementListResponse(
+                List.of(new AchievementResponseModel("JOURNEY_BEGINS", "UNLOCKED", LocalDateTime.now()))));
+
+        final String expectedTitle = "The journey of a thousand miles begins with one step";
+        final String expectedDescription = "Create your first todo";
+
+        assertUnlockedAchievement(viewModel, expectedTitle, expectedDescription);
+    }
+
+    @Test
+    public void handleSuccessfulResponse_unlockedDonezoAchievement() {
+        final var viewModel = new ToDoListViewModel();
+        final var presenter = new FetchAchievementListPresenter(viewModel);
+
+        presenter.handleSuccessfulResponse(new FetchAchievementListResponse(
+                List.of(new AchievementResponseModel("DONEZO", "UNLOCKED", LocalDateTime.now()))));
+
+        final String expectedTitle = "Donezo!";
+        final String expectedDescription = "Complete your first todo";
+
+        assertUnlockedAchievement(viewModel, expectedTitle, expectedDescription);
+    }
+
+    @Test
+    public void handleSuccessfulResponse_unlockedNewYearNewMeAchievement() {
+        final var viewModel = new ToDoListViewModel();
+        final var presenter = new FetchAchievementListPresenter(viewModel);
+
+        presenter.handleSuccessfulResponse(new FetchAchievementListResponse(
+                List.of(new AchievementResponseModel("NEW_YEAR_NEW_ME", "UNLOCKED", LocalDateTime.now()))));
+
+        final String expectedTitle = "New year, new me!";
+        final String expectedDescription = "Create a todo on new year";
+
+        assertUnlockedAchievement(viewModel, expectedTitle, expectedDescription);
+    }
+
+    @Test
+    public void handleSuccessfulResponse_unlockedItBurnsAchievement() {
+        final var viewModel = new ToDoListViewModel();
+        final var presenter = new FetchAchievementListPresenter(viewModel);
+
+        presenter.handleSuccessfulResponse(new FetchAchievementListResponse(
+                List.of(new AchievementResponseModel("IT_BURNS", "UNLOCKED", LocalDateTime.now()))));
+
+        final String expectedTitle = "Oh, it burns!";
+        final String expectedDescription = "Complete ten todos in one week";
+
+        assertUnlockedAchievement(viewModel, expectedTitle, expectedDescription);
+    }
+
+    @Test
+    public void handleSuccessfulResponse_unlockedThirdTimesTheCharmAchievement() {
+        final var viewModel = new ToDoListViewModel();
+        final var presenter = new FetchAchievementListPresenter(viewModel);
+
+        presenter.handleSuccessfulResponse(new FetchAchievementListResponse(
+                List.of(new AchievementResponseModel("THIRD_TIMES_THE_CHARM", "UNLOCKED", LocalDateTime.now()))));
+
+        final String expectedTitle = "Third time's the charm";
+        final String expectedDescription = "Reopen a todo for the third time";
+
+        assertUnlockedAchievement(viewModel, expectedTitle, expectedDescription);
+    }
+
+    @Test
+    public void handleSuccessfulResponse_unlockedAchievementHunterAchievement() {
+        final var viewModel = new ToDoListViewModel();
+        final var presenter = new FetchAchievementListPresenter(viewModel);
+
+        presenter.handleSuccessfulResponse(new FetchAchievementListResponse(
+                List.of(new AchievementResponseModel("ACHIEVEMENT_HUNTER", "UNLOCKED", LocalDateTime.now()))));
+
+        final String expectedTitle = "Achievement hunter";
+        final String expectedDescription = "Collect three achievements";
+
+        assertUnlockedAchievement(viewModel, expectedTitle, expectedDescription);
+    }
+
+    @Test
+    public void handleSuccessfulResponse_unlockedAllDoneAchievement() {
+        final var viewModel = new ToDoListViewModel();
+        final var presenter = new FetchAchievementListPresenter(viewModel);
+
+        presenter.handleSuccessfulResponse(new FetchAchievementListResponse(
+                List.of(new AchievementResponseModel("ALL_DONE", "UNLOCKED", LocalDateTime.now()))));
+
+        final String expectedTitle = "All done!";
+        final String expectedDescription = "Hooray, you completed all of your todos!";
+
+        assertUnlockedAchievement(viewModel, expectedTitle, expectedDescription);
+    }
+
+    @Test
+    public void handleSuccessfulResponse_unlockedLazyDogAchievement() {
+        final var viewModel = new ToDoListViewModel();
+        final var presenter = new FetchAchievementListPresenter(viewModel);
+
+        presenter.handleSuccessfulResponse(new FetchAchievementListResponse(
+                List.of(new AchievementResponseModel("LAZY_DOG", "UNLOCKED", LocalDateTime.now()))));
+
+        final String expectedTitle = "You lazy dog!";
+        final String expectedDescription = "Leave one of your todos open for three months";
+
+        assertUnlockedAchievement(viewModel, expectedTitle, expectedDescription);
+    }
+
+    /**
+     * Asserts an unlocked achievement.
+     *
+     * @param viewModel           The view model.
+     * @param expectedTitle       The expected title.
+     * @param expectedDescription The expected description.
+     */
+    private static void assertUnlockedAchievement(
+            final ToDoListViewModel viewModel,
+            final String expectedTitle,
+            final String expectedDescription) {
+        final var assertions = new SoftAssertions();
+
+        assertions.assertThat(viewModel.achievements.get()).isNotEmpty();
+        assertions.assertThat(viewModel.achievements.get().stream().findFirst().orElseThrow().title.get())
+                .isEqualTo(expectedTitle);
+        assertions.assertThat(viewModel.achievements.get().stream().findFirst().orElseThrow().description.get())
+                .isEqualTo(expectedDescription);
+        assertions.assertThat(viewModel.achievements.get().stream().findFirst().orElseThrow().unlocked.get())
+                .isNotNull();
+        assertions.assertThat(viewModel.achievements.get().stream().findFirst().orElseThrow().state.get())
+                .isEqualTo("UNLOCKED");
+
+        assertions.assertAll();
+    }
 }

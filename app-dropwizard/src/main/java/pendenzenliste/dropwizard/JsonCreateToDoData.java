99731862e887c327a3709fe38d800fdad52539d1
Dennis Reichenberg <9849919+SoylentBob@users.noreply.github.com>
2023-12-02 15:23:02 +0100

    Refactored Json Data structures to java records

--- app-dropwizard/src/main/java/pendenzenliste/dropwizard/JsonCreateToDoData.java
+++ app-dropwizard/src/main/java/pendenzenliste/dropwizard/JsonCreateToDoData.java
@@ -1,47 +1,15 @@
 package pendenzenliste.dropwizard;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * A data model for a json message used to create a todo.
+ *
+ * @param headline    The headline of the todo
+ * @param description The description of the todo.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class JsonCreateToDoData {
-
-    private final String headline;
-    private final String description;
-
-    /**
-     * Creates a new instance.
-     *
-     * @param headline    The headline.
-     * @param description The description.
-     */
-    @JsonCreator
-    public JsonCreateToDoData(@JsonProperty("headline") final String headline,
-                              @JsonProperty("description") final String description) {
-
-        this.headline = headline;
-        this.description = description;
-    }
-
-    /**
-     * Retrieves the headline of the todo.
-     *
-     * @return The headline.
-     */
-    public String getHeadline() {
-        return headline;
-    }
-
-    /**
-     * Retrieves the description of the todo.
-     *
-     * @return The description.
-     */
-    public String getDescription() {
-        return description;
-    }
+public record JsonCreateToDoData(@JsonProperty("headline") String headline,
+                                 @JsonProperty("description") String description) {
 }
--- app-dropwizard/src/main/java/pendenzenliste/dropwizard/JsonCreatedToDoData.java
+++ app-dropwizard/src/main/java/pendenzenliste/dropwizard/JsonCreatedToDoData.java
@@ -1,54 +1,13 @@
 package pendenzenliste.dropwizard;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 /**
  * A JSON based DTO that represents a created todos data.
+ *
+ * @param id The ID of the created todo.
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class JsonCreatedToDoData {
-
-    private final String id;
-
-    /**
-     * Creates a new instance.
-     *
-     * @param id The ID of the created todo.
-     */
-    @JsonCreator
-    public JsonCreatedToDoData(final String id) {
-        this.id = id;
-    }
-
-
-    /**
-     * Retrieves the ID.
-     *
-     * @return The ID.
-     */
-    public String getId() {
-        return id;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean equals(final Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        JsonCreatedToDoData that = (JsonCreatedToDoData) o;
-        return Objects.equals(id, that.id);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public int hashCode() {
-        return Objects.hash(id);
-    }
+public record JsonCreatedToDoData(@JsonProperty("id") String id) {
 }

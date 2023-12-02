4af22cb57dbf17c4e790171b559661e8f575da2a
Dennis Reichenberg <9849919+SoylentBob@users.noreply.github.com>
2023-12-02 13:02:45 +0100

    Removed System.out call in FetchToDoListPresenter

--- app-javafx/src/main/java/pendenzenliste/javafx/FetchToDoListPresenter.java
+++ app-javafx/src/main/java/pendenzenliste/javafx/FetchToDoListPresenter.java
@@ -1,77 +1,73 @@
 package pendenzenliste.javafx;
 
-import java.util.function.Function;
-
-import static java.util.Objects.requireNonNull;
-
 import pendenzenliste.todos.boundary.out.FetchToDoListFailedResponse;
 import pendenzenliste.todos.boundary.out.FetchToDoListOutputBoundary;
 import pendenzenliste.todos.boundary.out.FetchedToDoListResponse;
 import pendenzenliste.todos.boundary.out.ToDoListResponseModel;
 
+import java.util.function.Function;
+
+import static java.util.Objects.requireNonNull;
+
 /**
  * A presenter that can be used to handle the results of a fetch todo list request.
  */
-public class FetchToDoListPresenter implements FetchToDoListOutputBoundary
-{
-  private final ToDoListViewModel viewModel;
+public class FetchToDoListPresenter implements FetchToDoListOutputBoundary {
+    private static final System.Logger LOGGER = System.getLogger(FetchToDoListPresenter.class.getName());
 
-  /**
-   * Creates a new instance.
-   *
-   * @param viewModel The view model that should be used by this instance.
-   */
-  public FetchToDoListPresenter(final ToDoListViewModel viewModel)
-  {
-    this.viewModel = requireNonNull(viewModel, "The viewModel may not be null");
-  }
+    private final ToDoListViewModel viewModel;
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public void handleFailedResponse(final FetchToDoListFailedResponse response)
-  {
-    //TODO: Show some kind of error message
-    System.out.println(response.reason());
-  }
+    /**
+     * Creates a new instance.
+     *
+     * @param viewModel The view model that should be used by this instance.
+     */
+    public FetchToDoListPresenter(final ToDoListViewModel viewModel) {
+        this.viewModel = requireNonNull(viewModel, "The viewModel may not be null");
+    }
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public void handleSuccessfulResponse(final FetchedToDoListResponse response)
-  {
-    viewModel.todos.setAll(response.todos().stream().map(mapToViewModel()).toList());
-  }
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void handleFailedResponse(final FetchToDoListFailedResponse response) {
+        //TODO: Show some kind of error message
+        LOGGER.log(System.Logger.Level.ERROR, response.reason());
+    }
 
-  /**
-   * Maps the given response model to a DTO.
-   *
-   * @return The mapping function.
-   */
-  private static Function<ToDoListResponseModel, ToDoListItemViewModel> mapToViewModel()
-  {
-    return todo -> {
-      var viewModel = new ToDoListItemViewModel();
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void handleSuccessfulResponse(final FetchedToDoListResponse response) {
+        viewModel.todos.setAll(response.todos().stream().map(mapToViewModel()).toList());
+    }
 
-      viewModel.identity.set(todo.identity());
-      viewModel.headline.set(todo.headline());
-      viewModel.created.set(todo.created());
-      viewModel.lastModified.set(todo.lastModified());
-      viewModel.completed.set(todo.completed());
-      viewModel.state.set(todo.state());
+    /**
+     * Maps the given response model to a DTO.
+     *
+     * @return The mapping function.
+     */
+    private static Function<ToDoListResponseModel, ToDoListItemViewModel> mapToViewModel() {
+        return todo -> {
+            var viewModel = new ToDoListItemViewModel();
 
-      return viewModel;
-    };
-  }
+            viewModel.identity.set(todo.identity());
+            viewModel.headline.set(todo.headline());
+            viewModel.created.set(todo.created());
+            viewModel.lastModified.set(todo.lastModified());
+            viewModel.completed.set(todo.completed());
+            viewModel.state.set(todo.state());
 
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  public boolean isDetached()
-  {
-    return false;
-  }
+            return viewModel;
+        };
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean isDetached() {
+        return false;
+    }
 }

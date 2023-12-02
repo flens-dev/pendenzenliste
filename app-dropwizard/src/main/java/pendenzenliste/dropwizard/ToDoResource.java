351005bfcda63fab9fb95eed9239900fb3600426
Dennis Reichenberg <9849919+SoylentBob@users.noreply.github.com>
2023-12-02 09:35:25 +0100

    Added Javadoc to ToDoResource

--- app-dropwizard/src/main/java/pendenzenliste/dropwizard/ToDoResource.java
+++ app-dropwizard/src/main/java/pendenzenliste/dropwizard/ToDoResource.java
@@ -1,12 +1,12 @@
 package pendenzenliste.dropwizard;
 
+import pendenzenliste.todos.boundary.in.ToDoInputBoundaryFactoryProvider;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import static java.util.Objects.requireNonNull;
-
-import pendenzenliste.todos.boundary.in.ToDoInputBoundaryFactoryProvider;
 
 /**
  * A resource that can be used to access the todos.
@@ -14,71 +14,73 @@ import pendenzenliste.todos.boundary.in.ToDoInputBoundaryFactoryProvider;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 @Path("todos")
-public class ToDoResource
-{
-  private final ToDoInputBoundaryFactoryProvider provider;
+public class ToDoResource {
+    private final ToDoInputBoundaryFactoryProvider provider;
 
-  /**
-   * Creates a new instance.
-   *
-   * @param provider The provider.
-   */
-  public ToDoResource(final ToDoInputBoundaryFactoryProvider provider)
-  {
+    /**
+     * Creates a new instance.
+     *
+     * @param provider The provider.
+     */
+    public ToDoResource(final ToDoInputBoundaryFactoryProvider provider) {
 
-    this.provider = requireNonNull(provider, "The provider may not be null");
-  }
+        this.provider = requireNonNull(provider, "The provider may not be null");
+    }
 
-  /**
-   * Lists the todos.
-   *
-   * @return The response.
-   */
-  @GET
-  public Response list()
-  {
-    final var viewModel = new HttpResponseViewModel();
-    final var outputBoundaryFactory = new JsonToDoPresenterFactory(viewModel);
-    final var inputBoundaryFactory = provider.getInstance(outputBoundaryFactory);
+    /**
+     * Lists the todos.
+     *
+     * @return The response.
+     */
+    @GET
+    public Response list() {
+        final var viewModel = new HttpResponseViewModel();
+        final var outputBoundaryFactory = new JsonToDoPresenterFactory(viewModel);
+        final var inputBoundaryFactory = provider.getInstance(outputBoundaryFactory);
 
-    final var controller = new ToDoController(inputBoundaryFactory);
+        final var controller = new ToDoController(inputBoundaryFactory);
 
-    controller.list();
+        controller.list();
 
-    return viewModel.response;
-  }
+        return viewModel.response;
+    }
 
-  /**
-   * Fetches a specific todo by its ID.
-   *
-   * @param id The ID of the todo.
-   * @return The response.
-   */
-  @GET
-  @Path("{id}")
-  public Response fetch(@PathParam("id") final String id)
-  {
-    final var viewModel = new HttpResponseViewModel();
-    final var outputBoundaryFactory = new JsonToDoPresenterFactory(viewModel);
-    final var inputBoundaryFactory = provider.getInstance(outputBoundaryFactory);
+    /**
+     * Fetches a specific todo by its ID.
+     *
+     * @param id The ID of the todo.
+     * @return The response.
+     */
+    @GET
+    @Path("{id}")
+    public Response fetch(@PathParam("id") final String id) {
+        final var viewModel = new HttpResponseViewModel();
+        final var outputBoundaryFactory = new JsonToDoPresenterFactory(viewModel);
+        final var inputBoundaryFactory = provider.getInstance(outputBoundaryFactory);
 
-    final var controller = new ToDoController(inputBoundaryFactory);
+        final var controller = new ToDoController(inputBoundaryFactory);
 
-    controller.fetch(id);
+        controller.fetch(id);
 
-    return viewModel.response;
-  }
+        return viewModel.response;
+    }
 
-  @POST
-  public Response create(JsonCreateToDoData data) {
-    final var viewModel = new HttpResponseViewModel();
-    final var outputBoundaryFactory = new JsonToDoPresenterFactory(viewModel);
-    final var inputBoundaryFactory = provider.getInstance(outputBoundaryFactory);
+    /**
+     * Creates a new todo.
+     *
+     * @param data The data used to create the todo.
+     * @return The response.
+     */
+    @POST
+    public Response create(JsonCreateToDoData data) {
+        final var viewModel = new HttpResponseViewModel();
+        final var outputBoundaryFactory = new JsonToDoPresenterFactory(viewModel);
+        final var inputBoundaryFactory = provider.getInstance(outputBoundaryFactory);
 
-    final var controller = new ToDoController(inputBoundaryFactory);
+        final var controller = new ToDoController(inputBoundaryFactory);
 
-    controller.create(data);
+        controller.create(data);
 
-    return viewModel.response;
-  }
+        return viewModel.response;
+    }
 }
